67cae326ba0f33f9ba7fcd6558dd2392f0d6ea7e
Ujjawal Gupta <ujjawal.gupta.jsr@gmail.com>
2026-06-12 21:23:34 +0530

    Refactor SmartHomeMenu and enhance device status display

--- SmartHomeMenu.java
+++ SmartHomeMenu.java
@@ -2,90 +2,129 @@ import java.util.Scanner;
 
 // Base Class
 class Device {
-    String name;
-    boolean status;
+    protected String name;
+    protected boolean status;
 
-    Device(String name) {
+    public Device(String name) {
         this.name = name;
         this.status = false;
     }
 
-    void turnOn() {
-        status = true;
-        System.out.println(name + " is now ON");
+    public void turnOn() {
+        if (status) {
+            System.out.println(name + " is already ON.");
+        } else {
+            status = true;
+            System.out.println(name + " is now ON.");
+        }
     }
 
-    void turnOff() {
-        status = false;
-        System.out.println(name + " is now OFF");
+    public void turnOff() {
+        if (!status) {
+            System.out.println(name + " is already OFF.");
+        } else {
+            status = false;
+            System.out.println(name + " is now OFF.");
+        }
+    }
+
+    public void showStatus() {
+        System.out.println(name + " : " + (status ? "ON" : "OFF"));
     }
 }
 
 // Light Class
 class Light extends Device {
-    Light(String name) {
+    public Light(String name) {
         super(name);
+    }
+
+    @Override
+    public void turnOn() {
+        super.turnOn();
+        if (status) {
+            System.out.println("💡 Light is shining brightly.");
+        }
     }
 }
 
 // Fan Class
 class Fan extends Device {
-    Fan(String name) {
+    public Fan(String name) {
         super(name);
+    }
+
+    @Override
+    public void turnOn() {
+        super.turnOn();
+        if (status) {
+            System.out.println("🌀 Fan is spinning.");
+        }
     }
 }
 
 // Main Class
 public class SmartHomeMenu {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
         Light light = new Light("Living Room Light");
-        Fan fan = new Fan("Fan");
+        Fan fan = new Fan("Bedroom Fan");
 
         int choice;
 
         do {
-            System.out.println("\n--- Smart Home Menu ---");
+            System.out.println("\n========== SMART HOME ==========");
             System.out.println("1. Turn ON Light");
             System.out.println("2. Turn OFF Light");
             System.out.println("3. Turn ON Fan");
             System.out.println("4. Turn OFF Fan");
-            System.out.println("5. Exit");
+            System.out.println("5. Show Device Status");
+            System.out.println("6. Exit");
+            System.out.print("Enter Choice: ");
 
-            System.out.print("Enter your choice: ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid Input! Enter a number.");
+                sc.next();
+                continue;
+            }
+
             choice = sc.nextInt();
 
             switch (choice) {
                 case 1:
-                    System.out.println("Turning ON Light...");
                     light.turnOn();
                     break;
 
                 case 2:
-                    System.out.println("Turning OFF Light...");
                     light.turnOff();
                     break;
 
                 case 3:
-                    System.out.println("Turning ON Fan...");
                     fan.turnOn();
                     break;
 
                 case 4:
-                    System.out.println("Turning OFF Fan...");
                     fan.turnOff();
                     break;
 
                 case 5:
-                    System.out.println("Exiting...");
+                    System.out.println("\n----- DEVICE STATUS -----");
+                    light.showStatus();
+                    fan.showStatus();
+                    break;
+
+                case 6:
+                    System.out.println("Thank you for using Smart Home System.");
                     break;
 
                 default:
-                    System.out.println("Invalid choice!");
+                    System.out.println("Invalid Choice!");
             }
 
-        } while (choice != 5);
+        } while (choice != 6);
 
         sc.close();
     }
